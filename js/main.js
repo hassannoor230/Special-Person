819202a01615cba4eee5adcb6bb830723b817232
hassannoor230 <hassannoor2309@gmail.com>
2026-01-14 17:29:25 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('This is for you [Name]').split('')
+    const titles = ('This is for you Meri Jan 🥰❤️').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
